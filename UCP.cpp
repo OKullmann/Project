a5ece284debada225c1c5b6d1f07e0f8f9ff0c71
639909 <639909@gmail.com>
2014-02-19 19:01:42 +0000

    Update UCP.cpp
    Program takes in a file, and ensures that a file has been input from the command line

--- UCP.cpp
+++ UCP.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main(int argc, char *argv[]) {
-	printf("Blank start to project");
+	string line;
+	
+	if (argc !=2)
+		cout << "Usage: " << argv[0] << " <Input filename>" << endl; //Error for wrong input
+	else {
+		ifstream inFile (argv[1]);
+	
+		if(inFile.is_open())
+			cout << "file accepted" << endl;
+			line = inFile.getline(1);
+		else {
+			cout << "No good" << endl;
+		}
+	}
 }
