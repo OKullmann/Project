34a84e09d5f905c12351313aa36dbea3d2e5d8b5
639909 <639909@gmail.com>
2014-02-20 19:21:10 +0000

    Add Functionality
    + File is read to an array
    + Each clause has its own line in array
    + 0's at the end of clauses are ignored

--- UCP.cpp
+++ UCP.cpp
@@ -1,18 +1,40 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <sstream>
+
 using namespace std;
 
 int main(int argc, char *argv[]){
+	string pLine;
 	string line;
+	int clauseCount = 4;
 	
 	if (argc !=2) //If no file is entered for input
 		cout << "Usage: " << argv[0] << " <Input filename>" << endl; 
 	else {
 		ifstream inFile (argv[1]);
+		stringstream buffer;
+		ofstream outFile;
 	
 		if(inFile.is_open()){
 			cout << "** File accepted **" << endl;
-			cout << inFile.rdbuf() << endl; //Prints file contents to terminal
+			buffer << inFile.rdbuf();
+			line = buffer.str();
+			//Cursors for array
+			size_t pos0 = 0;
+			size_t pos1;
+			
+			string clause[3]; //Array to store clauses
+			for (int i=0; i<=(clauseCount); i++) {
+				pos1 = line.find("0", pos0); //Searches for the 0 at the end of the line
+				clause[i] = line.substr(pos0, (pos1 -pos0)); //Substring each clause minus 0
+				cout << clause[i];
+				pos0 = pos1+1;
+			}
+			cout << endl;
+			cout << endl <<"End of file" << endl;
+			
 		} else {
 			cout << "No good" << endl;
 		}
