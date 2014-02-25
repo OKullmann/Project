ba35d466338b605c767fba51ad548975012dade5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2014-02-25 12:50:13 +0000

    Removed all comments.
    Clean code!!

--- UCP.cpp
+++ UCP.cpp
@@ -8,7 +8,7 @@ int main(const int argc, const char* const argv[]) {
   std::string watched;
   const int clauseCount = 3;
 
-  if (argc !=2) //If no file is entered for input
+  if (argc !=2)
     std::cout << "Usage: " << "./UCP <Input filename>" << std::endl;
   else {
     const std::ifstream inFile(argv[1]);
@@ -18,18 +18,16 @@ int main(const int argc, const char* const argv[]) {
     if(inFile.is_open()) {
       buffer << inFile.rdbuf();
       line = buffer.str();
-      //Cursors for array
       size_t pos0 = 0;
       size_t pos1;
 
       outFile.open("output.txt");
-      //outFile << "c This file has had the Unit Clause Propagation applied to it" << std::endl;
       std::cout << " ** FILE ** " << std::endl;
-      std::string clause[255]; //Array to store clauses
+      std::string clause[255];
       for (int i = 0; i <= clauseCount; i++) {
-	pos1 = line.find("0", pos0); //Searches for the 0 at the end of the line
-	clause[i] = line.substr(pos0, pos1 -pos0 -1); //Substring each clause minus 0
-	std::cout << clause[i];//.length() <<std::endl;
+	pos1 = line.find("0", pos0);
+	clause[i] = line.substr(pos0, pos1 -pos0 -1);
+	std::cout << clause[i];
 	pos0 = pos1+1;
       }
       std::cout << std::endl << std::endl << "Solution" << std::endl;
