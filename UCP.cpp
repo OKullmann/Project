442ac0a6b2e9e466272662f2c1e511db5074cd26
Josh <639909@gmail.com>
2014-03-12 13:06:27 +0000

    Program sorts between comments and clauses, stores each in a separate vector

--- UCP.cpp
+++ UCP.cpp
@@ -4,11 +4,16 @@
 
 int main(const int argc, const char* const argv[]) {
   std::string line;
-  std::vector<std::string> file;	
+  std::string c("c");
+  std::vector<std::string> comments;
+  std::vector<std::string> clauses;	
   if (argc !=2)
     std::cout << "Usage: " << "./UCP <Input filename>\n";
   std::ifstream inFile(argv[1]);
+  std::cout << "Cool \n";
   while (std::getline(inFile, line)) {
-    file.push_back(line);
+    if(line.compare(0, c.length(), c)==0)
+      comments.push_back(line);
+    else(clauses.push_back(line));
   }
 }
