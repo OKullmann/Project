86dbdda58f7100703f8a12ed067eb128915de681
Josh <639909@gmail.com>
2014-03-12 13:06:01 +0000

    Program now stores file in a vector

--- UCP.cpp
+++ UCP.cpp
@@ -1,52 +1,14 @@
-#include <iostream>
 #include <fstream>
-#include <string>
-#include <sstream>
-#include <cstdlib>
-
-// TODOS:
-// 1. use std::vector
+#include <iostream>
+#include <vector>
 
 int main(const int argc, const char* const argv[]) {
   std::string line;
-  std::string propagated;
-  const int clauseCount = 3;
-
+  std::vector<std::string> file;	
   if (argc !=2)
     std::cout << "Usage: " << "./UCP <Input filename>\n";
-  else {
-    const std::ifstream inFile(argv[1]);
-    std::ofstream outFile;
-    std::stringstream buffer;
-
-    if(inFile.is_open()) {
-      buffer << inFile.rdbuf();
-      line = buffer.str();
-      std::size_t pos0 = 0;
-      std::size_t pos1;
-
-      outFile.open("output.txt");
-      std::cout << " ** FILE ** \n";
-      std::string clause[255];
-      for (int i = 0; i <= clauseCount; ++i) {
-        pos1 = line.find("0", pos0);
-        clause[i] = line.substr(pos0, pos1 -pos0 -1);
-        std::cout << clause[i];
-        pos0 = pos1+1;
-      }
-      std::cout << "\n\nSolution\n";
-
-      for (int j = 0; j <= clauseCount; ++j)
-        if(clause[j].length() == 1) {
-          propagated = clause[j];
-          std::cout << propagated;
-          clause[j] = "Currently being propagated";
-          for (int k = j; k <= 3; ++k)
-            if(clause[k].find(propagated)) clause[k] = "n";
-          std::cout << clause[j];
-        }
-      outFile.close();
-    }
-    else std::cout << "No good\n";
+  std::ifstream inFile(argv[1]);
+  while (std::getline(inFile, line)) {
+    file.push_back(line);
   }
 }
