b16bb196a067a1f969033dfd8c5520786c40e7d5
Josh <639909@gmail.com>
2014-03-12 15:53:35 +0000

    Added else (Line 10)to ensure program only runs if input file

--- UCP.cpp
+++ UCP.cpp
@@ -7,13 +7,15 @@ int main(const int argc, const char* const argv[]) {
   std::vector<std::string> clauses;	
   if (argc !=2)
     std::cout << "Usage: " << "./UCP <Input filename>\n";
-  std::ifstream inFile(argv[1]);
-  std::cout << "Cool \n";
-  std::string line;
-  while (std::getline(inFile, line)) {
-	std::string c("c");
-    if(line.compare(0, c.length(), c)==0)
-      comments.push_back(line);
-    else(clauses.push_back(line));
+  else{
+    std::ifstream inFile(argv[1]);
+    std::cout << "Cool \n";
+    std::string line;
+    while (std::getline(inFile, line)) {
+      std::string c("c");
+      if(line.compare(0, c.length(), c)==0)
+        comments.push_back(line);
+      else(clauses.push_back(line));
+    }
   }
 }
