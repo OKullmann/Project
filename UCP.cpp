4917b7973798d9a4a747fb3c52bb66913da8413c
Josh <639909@gmail.com>
2014-04-07 11:13:01 +0100

    Fixed program taking too many spaces
    Taking out the extra space would cause the program to think that there
    were extra unit clauses, when this was not the case

--- UCP.cpp
+++ UCP.cpp
@@ -54,11 +54,11 @@ int main(const int argc, const char* const argv[]) {
         int pos = propagator.find(dash);
         if(pos!=std::string::npos){
           propagator.replace(pos,dash.size(),"");
-          propagator = space + propagator + space;
+          propagator = space + propagator;
           secondClause = 0;
           }
         else{
-          propagator = space + dash + propagator + space;
+          propagator = space + dash + propagator;
           secondClause = 0;
           }
       }
